028cb22a41001c3f106cf43746d0b40e71dca917
m.zhuravlev <maks723@yandex.ru>
2021-05-20 22:23:14 +0300

    init

---
+++ desktop/src/com/gato/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gato.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gato.game.GatoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GatoGame(), config);
+	}
+}
---
+++ html/src/com/gato/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.gato.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.gato.game.GatoGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GatoGame();
+        }
+}

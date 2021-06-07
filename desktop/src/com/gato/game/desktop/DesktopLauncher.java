1ebb1957f5698915cb272939a36f3e5f6d3fee46
m.zhuravlev <maks723@yandex.ru>
2021-06-07 16:24:53 +0300

    temporary remove window resize

--- desktop/src/com/gato/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gato/game/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Gato");
         config.setWindowedMode(800, 480);
+        config.setResizable(false);
         new Lwjgl3Application(new GatoGame(), config);
     }
 }
